1b48abbb063163c0d9b5077132c30e330980cd7b
unknown <brijesh4799maurya@gmail.com>
2024-01-09 17:03:17 +0530

    initial commit

---
+++ src/main/java/com/app/raghu/runner/TestDataRunner.java
@@ -0,0 +1,17 @@
+package com.app.raghu.runner;
+
+import org.springframework.boot.CommandLineRunner;
+
+import com.app.raghu.bean.DatabaseCon;
+
+public class TestDataRunner implements CommandLineRunner{
+	private DatabaseCon con;
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
